ea84113a9b5cd3d2b74604132965c88112d6c513
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-04 15:17:16 -0500

    adding git aliases

---
+++ git/aliases.sh
@@ -0,0 +1,13 @@
+alias gc="git clone"
+alias gs="git status"
+alias ga="git add"
+alias gcm="git commit -m"
+alias gl="git log --pretty=format:'%h - %an, %ar: %s'"
+
+alias gpul="git pull origin"
+alias gpsh="git push origin"
+
+alias gb="git branch"
+alias gchk="git checkout"
+alias gm="git merge"
+alias gd="git branch -d"

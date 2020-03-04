e05787f10624e54b887010415f91e6aa618c8fdc
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-04 15:37:01 -0500

    adding docker aliases

---
+++ docker/aliases.sh
@@ -0,0 +1,16 @@
+alias dpul="docker pull"
+alias dpsh="docker push"
+
+alias dr="docker run"
+alias dx="docker exec"
+
+alias dils="docker image ls -a"
+alias dirm="docker image rm --force"
+
+alias dcls="docker container ls -a -s"
+alias dcrm="docker container rm --force"
+
+alias dbld="docker-compose up --build"
+alias dup="docker-compose up"
+alias dupd="docker-compose up -d"
+alias ddwn="docker-compose down"

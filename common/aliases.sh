95e5bd30a35938743b306cd0725f6a7d324bec80
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-04 15:03:54 -0500

    adding common aliases

---
+++ common/aliases.sh
@@ -0,0 +1,16 @@
+alias go="cd"
+alias bk="cd .."
+
+alias mv="mv"
+alias cp="cp"
+
+alias ls="ls"
+alias lsa="ls -a"
+
+alias mk="touch"
+alias mkdir="mkdir"
+
+alias rm="rm"
+alias rmf="rm -rf"
+
+alias cl="clear"

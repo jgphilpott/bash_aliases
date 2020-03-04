d78d2b18c5ffacb9fe76e2d942e251f8b6253156
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-04 15:22:14 -0500

    adding pip aliases

---
+++ pip/aliases.sh
@@ -0,0 +1,10 @@
+alias pls="pip list"
+alias pi="pip install"
+alias pui="pip uninstall"
+
+alias pfr="pip freeze > requirements.txt"
+alias pir="pip install -r requirements.txt"
+
+alias mkpenv="python -m venv venv"
+alias penvon="source venv/bin/activate"
+alias penvoff="deactivate"

c0866a4d0553db99a24dd146404f0334eb4ccc32
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-05 02:58:28 -0500

    Common Aliases Update

--- common/aliases.sh
+++ common/aliases.sh
@@ -1,16 +1,16 @@
 alias go="cd"
 alias bk="cd .."
 
-alias mv="mv"
-alias cp="cp"
-
 alias ls="ls"
 alias lsa="ls -a"
 
 alias mk="touch"
 alias mkdir="mkdir"
 
+alias mv="mv"
+alias cp="cp"
+
 alias rm="rm"
-alias rmf="rm -rf"
+alias rmdir="rm -rf"
 
 alias cl="clear"

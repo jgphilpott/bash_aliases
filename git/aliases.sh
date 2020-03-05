e93470a725149f4bfbfa130220fa55706253d73c
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-05 16:13:08 -0500

    Git Aliases Update

--- git/aliases.sh
+++ git/aliases.sh
@@ -4,10 +4,10 @@ alias ga="git add"
 alias gcm="git commit -m"
 alias gl="git log --pretty=format:'%h - %an, %ar: %s'"
 
+alias gb="git branch"
+alias gbmv="git checkout"
+alias gbm="git merge"
+alias gbrm="git branch -d"
+
 alias gpul="git pull origin"
 alias gpsh="git push origin"
-
-alias gb="git branch"
-alias gchk="git checkout"
-alias gm="git merge"
-alias gbd="git branch -d"

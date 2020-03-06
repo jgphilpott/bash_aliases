362adbd221cd6fdc9dc444291a7687565b1735e5
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-05 22:01:02 -0500

    PIP Aliases Update

--- pip/aliases.sh
+++ pip/aliases.sh
@@ -2,9 +2,9 @@ alias pls="pip list"
 alias pi="pip install"
 alias pui="pip uninstall"
 
-alias pir="pip install -r requirements.txt"
-alias pfr="pip freeze > requirements.txt"
-
 alias mkpenv="python -m venv venv"
 alias penvon="source venv/bin/activate"
-alias penvoff="deactivate"
+alias penvof="deactivate"
+
+alias pfr="pip freeze > requirements.txt"
+alias pir="pip install -r requirements.txt"

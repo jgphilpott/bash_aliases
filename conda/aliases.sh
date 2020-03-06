7f5d952f500a648ad63c9a4c12c0b69ff20a2729
Jacob Philpott <jacob.gary.philpott@gmail.com>
2020-03-06 10:56:21 -0500

    Conda Aliases Update

--- conda/aliases.sh
+++ conda/aliases.sh
@@ -2,11 +2,11 @@ alias cls="conda list"
 alias ci="conda install"
 alias cui="conda uninstall"
 
-alias cir="conda install --file requirements.txt"
-alias cfr="conda list --export > requirements.txt"
-
 alias cenvs="conda info --envs"
 alias mkcenv="conda create --name"
 alias rmcenv="conda env remove --name"
 alias cenvon="conda activate"
-alias cenvoff="conda deactivate"
+alias cenvof="conda deactivate"
+
+alias cfr="conda list --export > requirements.txt"
+alias cir="conda install --file requirements.txt"
